e748478f0f1b079e0c630b6a52735e3295188677
argierdfj <argierincon@gmail.com>
2020-10-01 21:24:01 -0500

    Actualizando vista home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
+import AddCharacter from "../../components/AddCharacter/AddCharacter";
+import SectionCharacterCard from "../../components/SectionCharacterCard/SectionCharacterCard.jsx";
 
 import "./Home.css";
 
-// SECCIÓN 6. VIDEO 69,70
+// SECCIÓN 6. VIDEO 69 MIN 3:44 CÓMO USAR EL REACTDOM.RENDER(), 70 MIN 12:20 - MANEJO DEL ESTADO
+// VIDEO 75 MIN 00:58
 
 const Home = () => {
-  const [characters, setCharacters] = useState(["Rick", "Morty", "Beth"]);
-
+  const [characters, setCharacters] = useState(["Rick"]);
   return (
     <section>
       <h2>Rick and Morty App</h2>
+      <AddCharacter setCharacters={setCharacters} />
       <hr />
       <div>
         {characters.map((character) => {
-          return <p key={character}>{character}</p>;
+          return <SectionCharacterCard key={character} char={character} />;
         })}
       </div>
     </section>

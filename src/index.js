7f6395344d02db42a413c3d3e23133cd596b2763
argierdfj <argierincon@gmail.com>
2020-10-01 21:23:06 -0500

    Actualizando elemento a renderizar

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import Home from './views/Home/Home';
 import './index.css';
 
-// SECCIÓN 4, VIDEO 34
-// const app = document.getElementById('app');
+// SECCIÓN 4, VIDEO 34 MIN 4:00 - CÓMO RENDERIZAR HTML EN REACT
+// const app = document.querySelector('app');
 
-ReactDOM.render( < Home / > , document.getElementById('app'));
+ReactDOM.render( < Home / > , document.querySelector('#app'));

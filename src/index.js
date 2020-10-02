2666318dff74c9f192519b27ccf2a2f8de15964b
argierdfj <argierincon@gmail.com>
2020-10-02 17:48:14 -0500

    Eliminando archivos inutilizados

--- src/index.js
+++ src/index.js
@@ -4,7 +4,4 @@ import ReactDOM from 'react-dom';
 import Home from './views/Home/Home';
 import './index.css';
 
-// SECCIÓN 4, VIDEO 34 MIN 4:00 - CÓMO RENDERIZAR HTML EN REACT
-// const app = document.querySelector('app');
-
 ReactDOM.render( < Home / > , document.querySelector('#app'));

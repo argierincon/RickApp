4e38b77bca0a10d95d223f99df69a5bc6a93a4df
argierdfj <argierincon@gmail.com>
2020-10-02 19:24:43 -0500

    Añadiendo código útil

--- src/components/SectionGifsCategory/SectionGifsCategory.jsx
+++ src/components/SectionGifsCategory/SectionGifsCategory.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const SectionGifsCategory = ({ category }) => {
+
+  // const fetchGifs = async (category) => {
+  //   const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+  //     category
+  //   )}&limit=10&api_key=CgPiOlDWJgWEGDo70vyz4ACQAac30Raj`;
+  //   const resp = await fetch(url);
+  //   const { data } = await resp.json();
+
+  //   const gifs = data.map((gif) => {
+  //     return {
+  //       id: gif.id,
+  //       title: gif.title,
+  //       url: gif.images?.downsized_medium.url,
+  //     };
+  //   });
+  //   setImages(gifs);
+  // };
+
   return (
     <section>
       <h3>{category}</h3>

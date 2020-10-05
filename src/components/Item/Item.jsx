f5f4e930327040c1eef7da3a6950abcf43ee1ad7
argierdfj <argierincon@gmail.com>
2020-10-05 12:37:54 -0500

    eliminando archivos inutilizados

--- src/components/Item/Item.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Item = ({ itemToDo }) => {
-  return <p>{itemToDo}</p>;
-};
-
-export default Item;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,23 +1,12 @@
-import React, { useState } from "react";
-import AddItem from "../../components/AddItem/AddItem";
-import Item from "../../components/Item/Item";
+import React from "react";
 
 import "./Home.css";
 
 const Home = () => {
-  const [tasks, setTasks] = useState([]);
-
   return (
     <section className="home">
-      <h2>TO DO LIST TEST</h2>
-      <AddItem setItemToDo={setTasks} />
+      <h2>?</h2>
       <hr />
-      <ol>
-        {tasks.map((tsk) => {
-          return <li>{tsk}</li>;
-        })}
-        <Item />
-      </ol>
     </section>
   );
 };
